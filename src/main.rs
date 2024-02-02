165f9ba8acf36a12ba9f5ccdef68e37d9c1f6df5
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2024-02-02 03:11:14 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use exitfailure::ExitFailure;
+use std::io::Write;
+use std::path::PathBuf;
+use structopt::StructOpt;
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use tree::{self, Counts};
+
+fn main() -> Result<(), ExitFailure> {
+  let args = tree::args::Opt::from_args();
+  let root: PathBuf = match &args.path {
+    Some(path) => path.to_path_buf(),
+    None => PathBuf::from("."),
+  };
+  let mut counts: Counts = Default::default();
+  let stdout = StandardStream::stdout(ColorChoice::Always);
+  let mut handle = stdout.lock();
+  handle.set_color(ColorSpec::new().set_fg(Some(Color::Blue)))?;
+  write!(handle, "{}", root.to_str().unwrap())?;
+  tree::walk_tree(
+    &mut handle,
+    &args,
+    root.to_str().unwrap(),
+    "",
+    1,
+    &mut counts,
+  )?;
+  if !args.no_report {
+    writeln!(
+      handle,
+      "\n{} directories, {} files",
+      counts.dirs, counts.files
+    )?;
+  }
+  Ok(())
+}
